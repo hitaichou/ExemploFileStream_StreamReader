44d797e974f1bbcc974b8f90018402bdeec7ee6c
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2020-08-17 18:59:28 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploFileStream_StreamReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemploFileStream_StreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
